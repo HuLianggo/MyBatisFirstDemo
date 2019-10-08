ea889a748caa6c588695fe70b61ceed55421c037
huliang <hulianghu@hotmail.com>
2019-10-08 23:08:41 +0800

    第一个版本

---
+++ src/cn/com/mybatis/test/MyBatisTest.java
@@ -0,0 +1,22 @@
+package cn.com.mybatis.test;
+
+import java.io.IOException;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import cn.com.mybatis.datasource.DataConnection;
+import cn.com.mybatis.po.User;
+
+public class MyBatisTest {
+	public DataConnection dataConn = new DataConnection();
+	@Test
+	public void TestSelect() throws IOException{
+		
+		SqlSession sqlSession = dataConn.getSqlSession();
+		User user = sqlSession.selectOne("test.findUserById", 1);
+		System.out.println("===="+user.getId()+"===="+user.getCity()+"===="+user.getGender()+"====");
+		sqlSession.close();
+		
+	}
+}
